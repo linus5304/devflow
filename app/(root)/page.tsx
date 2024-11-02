b7719007daef84a1365d6861b3ad9ebcacbbcc1c
linusbayere <linusbayere@gmail.com>
2024-11-02 13:46:20 -0400

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with github and google

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button>Logout</Button>
+      </form>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 import React from "react";
 
-import Navbar from "@/components/navigation/navbar";
+import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/toaster";
 import ThemeProvider from "@/context/theme";
 
 const inter = localFont({
@@ -24,26 +26,30 @@ export const metadata: Metadata = {
   icons: "/images/site-logo.svg",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
 }
